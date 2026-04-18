f34224367631678b5dfb35afd0b3fec7882867ea
Raushan Kumar <your_email@example.com>
2026-04-18 15:55:51 +0530

    Auto update

--- logic.js
+++ logic.js
@@ -1940,7 +1940,7 @@ function setupFirebaseListeners() {
                         showToast("Connection lost. Call ended.");
                         endCall(true);
                     }
-                }, 30000); // 3 seconds tolerance
+                }, 10000); // 3 seconds tolerance
             } else if (isOnline && isCallReconnecting) {
                 isCallReconnecting = false;
                 if (reconnectOverlay) reconnectOverlay.style.display = 'none';
@@ -1950,7 +1950,7 @@ function setupFirebaseListeners() {
                 }
             }
         }
-    }, 30000);
+    }, 1000);
 
     // 5. Typing Listener
     db.ref('typing').on('value', snapshot => {
